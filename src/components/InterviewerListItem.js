1bb2f11884e8e519cccbe9590a8c20a50aac9d12
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-14 16:55:47 -0400

    make interviewer name visible when selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,20 +1,25 @@
 import React from "react";
-import Classnames from 'classnames'
+import Classnames from "classnames";
 
-import './InterviewerListItem.scss'
+import "./InterviewerListItem.scss";
 
 export default function (props) {
+  const hidden = !props.selected && "interviewers__item--text--hidden";
   const interviewerClass = Classnames("interviewers__item", {
-    "interviewers__item--selected": props.selected
-  })
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+    <li
+      className={interviewerClass}
+      onClick={() => props.setInterviewer(props.name)}
+    >
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      <span className={hidden}>{props.name}</span>
     </li>
   );
 }
