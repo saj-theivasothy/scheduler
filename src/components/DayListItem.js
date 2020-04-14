61f53ab874296d5543e8934ab7b7a2778c66aea6
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-13 22:00:02 -0400

    make spots displayed dynamically and test this functionality

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,18 +1,26 @@
 import React from "react";
-import Classnames from 'classnames';
+import Classnames from "classnames";
 
-import './DayListItem.scss';
+import "./DayListItem.scss";
 
 export default function DayListItem(props) {
-  const dayClass = Classnames('day-list__item', {
-    'day-list__item--selected': props.selected, 
-    'day-list__item--full': !props.spots
-  })
+  const dayClass = Classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+  const formatSpots = () => {
+    return (
+      (!props.spots && "no spots") ||
+      (props.spots === 1 && "1 spot") ||
+      props.spots + " spots"
+    );
+  };
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
