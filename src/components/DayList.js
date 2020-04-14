39acf6f0f6e8d4131892b30226f64f85461f356e
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-13 22:42:06 -0400

    render DayListItem component using props, and test that the DayList component loads using storybook

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const DayList = props.days.map((day) => {
+    return (
+      <ul>
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      </ul>
+    );
+  });
+  return DayList;
+}
