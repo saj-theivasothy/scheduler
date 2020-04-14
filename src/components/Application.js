72172e05c5021b8f15babf7844449fe6f9b90614
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-13 23:20:24 -0400

    implement state to update the day chosen by user

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   const days = [
     {
       id: 1,
@@ -34,9 +36,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={(day) => console.log(day)}
-          />
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
