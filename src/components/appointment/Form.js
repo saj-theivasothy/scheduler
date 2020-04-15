8e7f481a0928794d772e0d50df92cd117796873f
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-15 00:03:52 -0400

    implement state for student name and interviewer

--- src/components/appointment/Form.js
+++ src/components/appointment/Form.js
@@ -3,16 +3,35 @@ import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
+
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  const handleSetName = (event) => {
+    setName(event.target.value)
+  }
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            value={name}
             type="text"
             placeholder="Enter Student Name"
+            onChange={(event) => handleSetName(event)}
             /*
           This must be a controlled component
         */
@@ -26,8 +45,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onConfirm}>Save</Button>
         </section>
       </section>
     </main>
