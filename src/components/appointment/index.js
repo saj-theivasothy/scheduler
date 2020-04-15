b9657baf0c89c691cac087320454f1d7b5e0f99f
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-15 09:54:13 -0400

    implement show and empty appointments logic

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -2,9 +2,17 @@ import React from "react";
 
 import "./styles.scss";
 
-export default function Appointment (props) {
+import Header from "components/appointment/Header";
+import Show from "components/appointment/Show";
+import Empty from "components/appointment/Empty";
 
+export default function Appointment(props) {
+  const appointmentStatus = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={console.log("onEdit")}
+  onDelete={console.log("onDelete")}/> : <Empty />;
   return (
-    <article className="appointment"></article>
-  )
+    <article className="appointment">
+      <Header time={props.time} />
+      {appointmentStatus}
+    </article>
+  );
 }
