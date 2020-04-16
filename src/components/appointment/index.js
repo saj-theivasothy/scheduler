48e15e5f27bb037bff8115153dee40b3266abec8
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-16 19:56:10 -0400

    render create form when new button is clicked

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -6,13 +6,14 @@ import Header from "components/appointment/Header";
 import Show from "components/appointment/Show";
 import Empty from "components/appointment/Empty";
 import useVisualMode from "hooks/useVisualMode.js";
+import Form from "components/appointment/Form"
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
 
-  console.log(props)
+  console.log(props);
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -20,14 +21,20 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={transition(CREATE)} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && }
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={props.onSave}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
