a8dc773743cf38c92d9ac20386675e4c4230bab7
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-16 18:30:06 -0400

    make use of useVisualMode hook to render empty component or show component

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -5,22 +5,29 @@ import "./styles.scss";
 import Header from "components/appointment/Header";
 import Show from "components/appointment/Show";
 import Empty from "components/appointment/Empty";
+import useVisualMode from "hooks/useVisualMode.js";
 
 export default function Appointment(props) {
-  const appointmentStatus = props.interview ? (
-    <Show
-      student={props.interview.student}
-      interviewer={props.interview.interviewer}
-      // onEdit={}
-      // onDelete={}
-    />
-  ) : (
-    <Empty />
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  console.log(props)
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
   );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {appointmentStatus}
+      {mode === EMPTY && <Empty onAdd={transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && }
     </article>
   );
 }
