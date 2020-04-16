014e65ef986c0293f3e3afb590831b2bae9c697d
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-15 21:23:54 -0400

    remove console log

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,6 +3,7 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
+  console.log(props.days)
   const DayList = props.days.map((day) => {
     return (
       <ul>
