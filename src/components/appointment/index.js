34c95aade4b28712e45dd729320cabb52f602849
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-17 17:27:26 -0400

    add save function to call the bookInterview function and transition the visual mode to show the book appointment

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -7,15 +7,30 @@ import Show from "components/appointment/Show";
 import Empty from "components/appointment/Empty";
 import useVisualMode from "hooks/useVisualMode.js";
 import Form from "components/appointment/Form"
+import Status from "./Status";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING"
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    transition(SAVING);
+
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    .catch(err => console.error(err))
+  }  
 
   return (
     <article className="appointment">
@@ -30,9 +45,12 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onSave={props.onSave}
+          onSave={save}
           onCancel={() => back()}
         />
+      )}
+      {mode === SAVING && (
+        <Status message="SAVING" />
       )}
     </article>
   );
