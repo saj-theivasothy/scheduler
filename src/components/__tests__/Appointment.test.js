6298b16dd9f9e749c6a1249d01f40f9d9d7a70e7
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-20 18:14:29 -0400

    add test to check if appointment component renders without crashing

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -17,6 +17,8 @@ import Application from "components/Application";
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
-});
+describe("Appointment component", () => {
+  it("renders without crashing", () => {
+    render(<Application />);
+  });
+})
