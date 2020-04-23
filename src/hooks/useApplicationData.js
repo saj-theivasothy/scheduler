c75073dc44aba3f29b719ae4b490a8cfb2753d35
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-23 16:54:23 -0400

    implement web socket and refactor code

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,61 @@
 import { useReducer, useEffect } from "react";
 import Axios from "axios";
 
+
 const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
+
+const DELETE = "DELETE";
+const SAVE = "SAVE";
+
+function updateSpots(state, action = null) {
+  const days = state.days.map((day) => {
+    if (day.name === state.day) {
+      const updatedSpots =
+      action === SAVE
+      ? day.spots - 1
+      : action === DELETE
+      ? day.spots + 1
+      : day.spots;
+      return { ...day, spots: updatedSpots };
+    }
+    return day;
+  });
+  return days;
+}
+
+function reducer(state, action) {
+  if (action.type === SET_DAY) {
+    return { ...state, day: action.day };
+  }
+  if (action.type === SET_APPLICATION_DATA) {
+    return {
+      ...state,
+      days: action.days,
+      appointments: action.appointments,
+      interviewers: action.interviewers,
+    };
+  }
+  if (action.type === SET_INTERVIEW) {
+    const appointment = {
+      ...state.appointments[action.appointmentId],
+      interview: { ...action.interview },
+    };
+    
+    const appointments = {
+      ...state.appointments,
+      [action.appointmentId]: appointment,
+    };
+    
+    const days = action.edit
+    ? updateSpots(state)
+    : action.DELETE
+    ? updateSpots(state, DELETE)
+    : updateSpots(state, SAVE);
+    return { ...state, appointments: appointments, days: days };
+  }
+}
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
@@ -12,80 +64,49 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {},
   });
-
-  function reducer(state, action) {
-    if (action.type === SET_DAY) {
-      return { ...state, day: action.day };
-    }
-    if (action.type === SET_APPLICATION_DATA) {
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers,
-      };
-    }
-    if (action.type === SET_INTERVIEW) {
-      return { ...state, appointments: action.appointments, days: action.days };
-    }
-  }
-
+  
   const setDay = (day) => dispatch({ type: SET_DAY, day });
-
+  
   useEffect(() => {
-    Promise.all([
-      Axios.get("/api/days"),
-      Axios.get("/api/appointments"),
-      Axios.get("/api/interviewers"),
-    ])
-      .then((all) => {
-        const days = all[0].data;
-        const appointments = all[1].data;
-        const interviewers = all[2].data;
+    const webSocket = new WebSocket("ws://localhost:8001");
+    webSocket.onopen = () => {
+      webSocket.onmessage = (event) => {
+        console.log("Message received", event.data);
+      };
+      console.log("connection opened");
+      Promise.all([
+        Axios.get("/api/days"),
+        Axios.get("/api/appointments"),
+        Axios.get("/api/interviewers"),
+      ])
+        .then((all) => {
+          const days = all[0].data;
+          const appointments = all[1].data;
+          const interviewers = all[2].data;
 
-        dispatch({
-          type: SET_APPLICATION_DATA,
-          days,
-          appointments,
-          interviewers,
-        });
-      })
-      .catch((err) => console.error(err));
+          dispatch({
+            type: SET_APPLICATION_DATA,
+            days,
+            appointments,
+            interviewers,
+          });
+        })
+        .catch((err) => console.error(err));
+    };
   }, []);
 
-  function updateSpots(action) {
-    const days = state.days.map((day) => {
-      if (day.name === state.day) {
-        const updatedSpots =
-          action === "save"
-            ? day.spots - 1
-            : action === "delete"
-            ? day.spots + 1
-            : day.spots;
-        return { ...day, spots: updatedSpots };
-      }
-      return day;
-    });
-    return days;
-  }
-
   function bookInterview(appointmentId, interview, edit) {
-    const appointment = {
-      ...state.appointments[appointmentId],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [appointmentId]: appointment,
-    };
-
     return Axios.put(`/api/appointments/${appointmentId}`, {
       interview: interview,
     })
       .then((res) => {
-        const days = edit ? updateSpots("edit") : updateSpots("save");
-       
-        dispatch({ type: SET_INTERVIEW, appointments, days });
+        dispatch({
+          type: SET_INTERVIEW,
+          appointmentId,
+          interview,
+          edit,
+        });
+        console.log(state.days)
         return res;
       })
       .catch((err) => {
@@ -94,21 +115,16 @@ export default function useApplicationData() {
   }
 
   function cancelInterview(appointmentId) {
-    const appointment = {
-      ...state.appointments[appointmentId],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [appointmentId]: appointment,
-    };
-
     return Axios.delete(`/api/appointments/${appointmentId}`, {
       interview: null,
     })
       .then((res) => {
-        const days = updateSpots("delete");
-        dispatch({ type: SET_INTERVIEW, appointments, days });
+        dispatch({
+          type: SET_INTERVIEW,
+          appointmentId,
+          interview: null,
+          DELETE,
+        });
         return res;
       })
       .catch((err) => {
