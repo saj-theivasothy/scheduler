a1f878ff42df23ae36f84e4abd5ec2bc27208f1c
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-22 20:45:18 -0400

    add end-to-end testing to verify add/edit/delete interview functionality

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,13 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("it should navigate to Tuesday", () => {
+    cy.visit("/");
+    
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
