dde8015c47184699c39053fd4b37d8983d3ddeb0
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-24 13:07:34 -0400

    remove false parameter from assertion

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -64,7 +64,7 @@ describe("Form", () => {
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1, false);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   it("calls onCancel and resets the input field", () => {
