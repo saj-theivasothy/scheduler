9d194aba8ff7bc9dacf563b096ba20e2c5fffa0d
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-24 18:39:15 -0400

    remove whitespace

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import axios from "axios"
 
 import "index.scss";
-
 import Application from "components/Application";
 
 if (process.env.REACT_APP_API_BASE_URL) {
