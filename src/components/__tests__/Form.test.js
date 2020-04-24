f5e18e68c675be7120249a9db47c1e447a03595e
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-04-24 13:14:35 -0400

    remove unused import

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -3,7 +3,6 @@ import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/appointment/Form";
-import { prettyDOM, getByAltText } from "@testing-library/dom";
 
 afterEach(cleanup);
 
